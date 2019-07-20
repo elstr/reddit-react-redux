845ae6193b9fe53bf51bc02d3730693de9a08f79
elstr <eleonora.lester@gmail.com>
2019-07-19 21:53:10 -0300

    feat: Get data from json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { Component } from 'react'
+import DATA from './data/top.json'
 import './App.css'
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+export default class App extends Component {
+  componentDidMount() {
+    const posts = DATA.data.children
+    console.log({posts})
+  }
+  render() {
+    return <div />
+  }
 }
-
-export default App
