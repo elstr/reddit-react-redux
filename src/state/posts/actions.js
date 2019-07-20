b24971aaffdc2ce5e62d251861b993a60e0e2414
elstr <eleonora.lester@gmail.com>
2019-07-19 23:14:49 -0300

    feat: Adds load posts action and reducer fn

--- src/state/posts/actions.js
+++ src/state/posts/actions.js
@@ -1,4 +1,14 @@
+export const LOAD_POSTS = 'LOAD_POSTS'
 export const DISMISS_POST = 'DISMISS_POST'
+
+export function loadPosts(posts) {
+  return {
+    type: LOAD_POSTS,
+    payload: {
+      posts
+    }
+  }
+}
 export function dismissPost(postId) {
   return {
     type: DISMISS_POST,
--- src/state/posts/reducer.js
+++ src/state/posts/reducer.js
@@ -1,12 +1,17 @@
-import { DISMISS_POST } from './actions'
+import { LOAD_POSTS, DISMISS_POST} from './actions'
 
 const initialState = {
-  list: [],
+  list: {},
   showDetail: false
 }
 
 export const posts = (state = initialState, action) => {
   switch (action.type) {
+    case LOAD_POSTS:
+      return {
+        ...state,
+        list: action.payload.posts
+      }
     case DISMISS_POST:
       return state
     default:
