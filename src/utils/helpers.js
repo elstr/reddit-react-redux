f6571817e36c40190419608fca0deeb34be5b3fc
elstr <eleonora.lester@gmail.com>
2019-07-21 12:44:29 -0300

    feat: Adds helper functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,7 +4,23 @@ const formatPosts = posts =>
     return list
   }, {})
 
+const patchPosts = (posts, post) => {
+  const updatedList = Object.assign({}, posts)
+  updatedList[post.id] = post
+  return updatedList
+}
+
+const setPostRead = post => Object.assign({}, post, { visited: true })
+
+const dismissPost = (posts, postId) => {
+  const filteredList = Object.assign({}, posts)
+  delete filteredList[postId]
+  return filteredList
+}
 
 module.exports = {
-    formatPosts
+  formatPosts,
+  setPostRead,
+  patchPosts,
+  dismissPost
 }
