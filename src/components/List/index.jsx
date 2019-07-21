13cb05b2009365e04d0fba976740881c70560305
elstr <eleonora.lester@gmail.com>
2019-07-21 12:23:20 -0300

    feat: Adds card rendering

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { readPost, dismissPost } from '../../state/posts/actions'
+import Card from '../Card'
+import "./style.css"
 
 class List extends Component {
+  handleRead = id => this.props.readPost(id)
+  handleDismiss = id => this.props.dismissPost(id)
   render() {
     const { posts } = this.props
-    return <div>{Object.keys(posts).map(k => console.log(posts[k]))}</div>
+    return (
+      <div className="list-container">
+        {Object.keys(posts).map(k => (
+          <Card key={posts[k].id} post={posts[k]} handleRead={this.handleRead} handleDismiss={this.handleDismiss} />
+        ))}
+      </div>
+    )
   }
 }
 
@@ -12,7 +23,13 @@ const mapStateToProps = state => ({
   posts: state.posts.list
 })
 
+const mapDispatchToProps = dispatch => ({
+  readPost: postId => dispatch(readPost(postId)),
+  dismissPost: postId => dispatch(dismissPost(postId)),
+})
+
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(List)
