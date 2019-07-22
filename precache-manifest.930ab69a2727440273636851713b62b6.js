97d3f1a9a8a561bab57ba82986b167de64b1bc38
elstr <eleonora.lester@gmail.com>
2019-07-22 18:13:51 -0300

    Updates

---
+++ precache-manifest.930ab69a2727440273636851713b62b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42917813e37725cbbe8816f66fca7ffc",
+    "url": "/reddit-react-redux/index.html"
+  },
+  {
+    "revision": "b22f3208abd27887737f",
+    "url": "/reddit-react-redux/static/css/main.89f3a131.chunk.css"
+  },
+  {
+    "revision": "4897e648f78a3a642fca",
+    "url": "/reddit-react-redux/static/js/2.61f68910.chunk.js"
+  },
+  {
+    "revision": "b22f3208abd27887737f",
+    "url": "/reddit-react-redux/static/js/main.45713973.chunk.js"
+  },
+  {
+    "revision": "da93a9d38c6d44136f1a",
+    "url": "/reddit-react-redux/static/js/runtime~main.2ad48622.js"
+  },
+  {
+    "revision": "ff0d9acdfce6979b87904c7c2b44a9bc",
+    "url": "/reddit-react-redux/static/media/left-arrow.ff0d9acd.svg"
+  },
+  {
+    "revision": "44ac022f54cfa691b3a83fb2dbff0775",
+    "url": "/reddit-react-redux/static/media/remove-button.44ac022f.svg"
+  },
+  {
+    "revision": "b25e09b12ca3f012c0eef4068fb7c1a7",
+    "url": "/reddit-react-redux/static/media/right-arrow.b25e09b1.svg"
+  }
+]);
